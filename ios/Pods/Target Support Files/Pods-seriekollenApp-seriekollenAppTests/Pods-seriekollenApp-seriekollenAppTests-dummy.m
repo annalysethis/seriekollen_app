a4219bd0c59e2b2e0bae7238175beb31ac7c78f9
annalysethis <annalys01@gmail.com>
2021-05-24 12:44:38 +0200

    deleted pods and added gitignore

--- ios/Pods/Target Support Files/Pods-seriekollenApp-seriekollenAppTests/Pods-seriekollenApp-seriekollenAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_seriekollenApp_seriekollenAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_seriekollenApp_seriekollenAppTests
-@end
--- ios/Pods/Target Support Files/Pods-seriekollenApp/Pods-seriekollenApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_seriekollenApp : NSObject
-@end
-@implementation PodsDummy_Pods_seriekollenApp
-@end
